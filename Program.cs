0fad92498999b8bf1e9f251581297503ae32b2b6
Todor <tdr.ktc@gmail.com>
2023-02-15 14:27:30 +0100

    CRUD

--- Program.cs
+++ Program.cs
@@ -1,3 +1,8 @@
+global using API.Models;
+global using API.Services.CharacterService;
+global using API.Dtos.Character;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +11,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddAutoMapper(typeof(Program).Assembly);
+builder.Services.AddScoped<ICharacterService, CharacterService>();
 
 var app = builder.Build();
 
